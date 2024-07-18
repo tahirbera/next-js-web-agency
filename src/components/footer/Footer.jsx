75fae8434124432922d3455c8f0b4e0d9280ca65
tahirbera <tahirberakeskin@gmail.com>
2024-07-18 15:57:59 +0300

    "last-of"

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>lamadev</div>
+      <div className={styles.logo}>BeraDev</div>
       <div className={styles.text}>
-        Lama creative thoughts agency © All rights reserved.
+        BeraDev creative thoughts agency © All rights reserved.
       </div>
     </div>
   );
